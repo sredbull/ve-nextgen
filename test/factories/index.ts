8bcfb6be5b99ddc6b4d8a4ee7fe1c5e399d37b13
Sven Roodbol <roodbol.sven@gmail.com>
2022-03-14 13:39:33 +0100

    updated test suite

--- test/factories/index.ts
+++
@@ -1,5 +0,0 @@
-import usersFactory from 'test/factories/user';
-import ordersFactory from 'test/factories/order';
-import thingsFactory from 'test/factories/thing';
-
-export { usersFactory, thingsFactory, ordersFactory };
--- test/factories/thing.ts
+++
@@ -1,8 +0,0 @@
-import { Factory } from 'fishery';
-import * as faker from 'faker';
-
-import { CreateThingDto } from 'src/server/app/dtos/create-thing.dto';
-
-export default Factory.define<CreateThingDto>(() => ({
-  name: faker.lorem.words(),
-}));
