385ca54059898b09b78ad181bccc3f355bf03dc2
Sven Roodbol <roodbol.sven@gmail.com>
2022-03-14 21:37:56 +0100

    removed cognito

--- src/server/app/auth/auth.module.ts
+++ src/server/app/auth/auth.module.ts
@@ -4,16 +4,9 @@ import { PassportModule } from '@nestjs/passport';
 import { GoogleOauthModule } from './google/google-oauth.module';
 import { JwtAuthModule } from './jwt/jwt-auth.module';
 import { AuthController } from './auth.controller';
-import { CognitoOauthModule } from './cognito/cognito-oauth.module';
 
 @Module({
   controllers: [AuthController],
-  imports: [
-    UserModule,
-    PassportModule,
-    GoogleOauthModule,
-    JwtAuthModule,
-    CognitoOauthModule,
-  ],
+  imports: [UserModule, PassportModule, GoogleOauthModule, JwtAuthModule],
 })
 export class AuthModule {}
